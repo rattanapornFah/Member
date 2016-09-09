fe484582dbe049b1256fe9fa17de671d9e84d185
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-09-09 09:57:47 +0700

    add template

---
+++ Member/Member/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Member.MvcApplication" Language="C#" %>
---
+++ Member/Member/MemberJS/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('Member', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        .when("/", {
+            templateUrl: "Home/Index"
+        })
+        .otherwise({
+            templateUrl: "Home/Index"
+        })
+});
