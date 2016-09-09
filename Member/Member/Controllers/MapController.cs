25781ff36ab8316733c4a86e1a2b14a37252e6bd
umapornPoy <p.tunlang7@gmail.com>
2016-09-09 11:17:22 +0700

    add map

---
+++ Member/Member/Controllers/MapController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Member.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Map()
+        {
+            return View();
+        }
+        public ActionResult Map2()
+        {
+            return View();
+        }
+        public ActionResult MapByName()
+        {
+            return View();
+        }
+    }
+}
