97bde7a2fedc63bed293fb7c174530436ec712ef
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-09-13 10:51:26 +0700

    delete /

--- Member/Member/MemberJS/app.js
+++ Member/Member/MemberJS/app.js
@@ -4,7 +4,7 @@ app.config(function ($routeProvider) {
     $routeProvider
 
         .when("/", {
-            templateUrl: "Login/login"
+            templateUrl: "Home/Index"
         })
          .when("/Map", {
              templateUrl: "Map/Map"
