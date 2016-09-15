fddbca084ac55c687974f544947baa303bf3e914
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-09-15 13:00:18 +0700

    add point.js

--- Member/Member/MemberJS/login.js
+++ Member/Member/MemberJS/login.js
@@ -1,13 +1,28 @@
-﻿app.controller('LoginController', ['$scope', function ($scope) {
+﻿app.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
     $scope.register = {};
-    //--------------------------- Log in---------------------------------------------------//
-    $scope.addMember = function (login) {
-        console.log(login)
-        var apiName = 'http://www.vtec-system.com:8080/LoyaltyApi/Member/GetMemberDataFromUserNamePassword?' +
-        'merchantId={' +merchantId+ '}&'
-        'memberUserName={' +memberUserName+ '}&'
-        'memberPassword = {' +memberPassword+ '}'
-            
 
+    //--------------------------- Log in---------------------------------------------------//
+    $scope.login = function (login) {
+
+          var apiName = 'http://www.vtec-system.com:8080/LoyaltyApi/Member/GetMemberDataFromUserNamePassword?' +
+        'merchantId=1&'+
+        'memberUserName=' + login.username + '&'+
+        'memberPassword=' + login.password + ''
+      
+         
+         $http.get(apiName)
+            .success(function (data) {
+                if (data.status == 0) {
+                    $scope.memberDetails = data.dataResult;
+                    localStorage.setItem('memberDetails', JSON.stringify($scope.memberDetails))
+                    parent.location = '/#/Point';
+                    //console.log(data)
+                } else {
+                    alert(data.dataResult)
+                    //console.log(data)
+                }
+                
+            })
     };
+  
 }])
---
+++ Member/Member/MemberJS/point.js
@@ -0,0 +1,11 @@
+﻿app.controller('PointController', ['$scope', '$http', function ($scope, $http) {
+    var apiName = 'http://www.vtec-system.com:8080/LoyaltyApi/Member/GetMemberCardsFromMemberId?merchantId=1&memberId=8'
+    $http.get(apiName).
+        success(function (data) {
+            $scope.memberDetails = data.dataResult;
+            console.log($scope.memberDetails)
+    })
+    //var memberDetails = localStorage.getItem('memberDetails');
+    //$scope.memberDetails = JSON.parse(memberDetails);
+    //console.log(JSON.parse(memberDetails))
+}])
