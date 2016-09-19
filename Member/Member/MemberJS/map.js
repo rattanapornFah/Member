76fa2feb8c4ca033920228bf8b1bd16e42d43515
umapornPoy <p.tunlang7@gmail.com>
2016-09-19 18:12:12 +0700

    update map

--- Member/Member/MemberJS/map.js
+++ Member/Member/MemberJS/map.js
@@ -1,5 +1,6 @@
 ﻿app.controller('MapController', ['$scope', '$http', function ($scope, $http) {
 
+
     $http.get("http://www.vtec-system.com:8080/LoyaltyApi/Stores/GetListAllStoresOfBrand?merchantId=1&brandId=1").success(function (data) {
         $scope.myWelcome = data.dataResult;
         console.log($scope.myWelcome);
