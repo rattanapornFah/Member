e7e4a57c3609c72a8764b49b5e3923b008eaa171
umapornPoy <p.tunlang7@gmail.com>
2016-09-22 18:04:16 +0700

    update infor image slide

--- Member/Member/MemberJS/js-image-slider.js
+++ Member/Member/MemberJS/js-image-slider.js
@@ -1,23 +1,39 @@
-$( window ).load( function() {
+var $first = $('li1:first', 'ul1'),
+      $last = $('li1:last', 'ul1');
 
-    $( '.sldr' ).each( function() {
-        var th = $( this );
-        th.sldr({
-            focalClass    : 'focalPoint',
-            offset        : th.width() / 2,
-            sldrWidth     : 'responsive',
-            nextSlide     : th.nextAll( '.sldr-nav.next:first' ),
-            previousSlide : th.nextAll( '.sldr-nav.prev:first' ),
-            selectors     : th.nextAll( '.selectors:first' ).find( 'li' ),
-            toggle        : th.nextAll( '.captions:first' ).find( 'div' ),
-            sldrInit      : sldrInit,
-            sldrStart     : sldrStart,
-            sldrComplete  : sldrComplete,
-            sldrLoaded    : sldrLoaded,
-            sldrAuto      : true,
-            sldrTime      : 2000,
-            hasChange     : true
-        });
-    });
+//$("#next").click(function () {
+//    var $next,
+//        $selected = $(".selected");
+//    // get the selected item
+//    // If next li is empty , get the first
+//    $next = $selected.next('li').length ? $selected.next('li') : $first;
+//    $selected.removeClass("selected").fadeOut('fast');
+//    $next.addClass('selected').fadeIn('fast');
+//});
 
-});
+//$("#prev").click(function () {
+//    var $prev,
+//        $selected = $(".selected");
+//    // get the selected item
+//    // If prev li is empty , get the last
+//    $prev = $selected.prev('li').length ? $selected.prev('li') : $last;
+//    $selected.removeClass("selected").fadeOut('fast');
+//    $prev.addClass('selected').fadeIn('fast');
+//});
+
+var time = 2000;
+var tid = setTimeout(timer, time);
+function timer() {
+    var $next,
+    $selected = $(".selected");
+    // get the selected item
+    // If next li is empty , get the first
+    $next = $selected.next('li1').length ? $selected.next('li1') : $first;
+    $selected.removeClass("selected").fadeOut('fast');
+    $next.addClass('selected').fadeIn('fast');
+    tid = setTimeout(timer, time); // repeat myself
+}
+function abortTimer() { // to be called when you want to stop the timer
+    clearTimeout(tid);
+}
+
