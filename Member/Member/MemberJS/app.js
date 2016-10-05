47acacd2bf4dbda19c422af8eb7357ae5b365bd3
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-10-05 14:31:19 +0700

    add barcode and qr code

--- Member/Member/MemberJS/app.js
+++ Member/Member/MemberJS/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('Member', ['ngRoute', 'ui.bootstrap', 'pascalprecht.translate']);
+﻿var app = angular.module('Member', ['ngRoute', 'ui.bootstrap', 'pascalprecht.translate','barcodeGenerator', 'ja.qr']);
 app.config(function ($routeProvider) {
     $routeProvider
 
